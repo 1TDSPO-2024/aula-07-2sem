d770ce6035150426eab0593f32321e1860f16c5d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-28 22:21:06 -0300

    Utilizando parametros com useParams e criando lista de objetos.

--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,13 +1,13 @@
 
 
-export default function Produtos(){
+export default function Produtos() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Olá, mundo sou o Produtos!</h1>
+    </div>
+  );
+}
