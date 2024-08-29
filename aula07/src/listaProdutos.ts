ca025067a47f8cb784bf30c474cfa459b7ef8caa
FrancescoDiBenedetto <francescomdibe@gmail.com>
2024-08-28 23:03:02 -0300

    chamando a lista

--- aula07/src/listaProdutos.ts
+++ aula07/src/listaProdutos.ts
@@ -1,4 +1,4 @@
-type Lista = {
+export type Lista = {
     id: number;
     nome: string;
     preco: number;
@@ -9,8 +9,8 @@ type Lista = {
 
 export let listaProdutos: Lista[] = [
     { id: 1, nome: 'mouse', preco: 40.45, qtd: 10, descricao: 'Mouse optico', imagem: 'mouse.jpg' },
-    { id: 1, nome: 'teclado mecanico', preco: 489.00, qtd: 5, descricao: 'Teclado Gamer', imagem: 'teclado.jpg' },
-    { id: 1, nome: 'Monitor Curvo', preco: 2895.90, qtd: 3, descricao: 'Monitor 32"', imagem: 'monitor.jpg' },
-    { id: 1, nome: 'Cadeira', preco: 2589.77, qtd: 3, descricao: 'Cadeira Gamer', imagem: 'cadeira.jpg' },
-    { id: 1, nome: 'Mouse Pad', preco: 75.00, qtd: 32, descricao: 'Mouse Pad Tematico', imagem: 'mouse-pad.jpg' },
+    { id: 2, nome: 'teclado mecanico', preco: 489.00, qtd: 5, descricao: 'Teclado Gamer', imagem: 'teclado.jpg' },
+    { id: 3, nome: 'Monitor Curvo', preco: 2895.90, qtd: 3, descricao: 'Monitor 32"', imagem: 'monitor.jpg' },
+    { id: 4, nome: 'Cadeira', preco: 2589.77, qtd: 3, descricao: 'Cadeira Gamer', imagem: 'cadeira.jpg' },
+    { id: 5, nome: 'Mouse Pad', preco: 75.00, qtd: 32, descricao: 'Mouse Pad Tematico', imagem: 'mouse-pad.jpg' },
 ];
--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,13 +1,48 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
 
+export default function Produtos() {
 
-export default function Produtos(){
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  return (
+    <div>
+      <h1>Olá, mundo sou o Produtos!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Preco</th>
+            <th>Qtd</th>
+            <th>Descricao</th>
+            <th>Imagem</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.descricao}</td>
+              <td>{produto.imagem}</td>
+              <td><Link to={`/editar/produtos/${produto.id}`}>Editar</Link> </td>
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={6}>
+              <span>Quantidade de Produtos{listaProdutos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
