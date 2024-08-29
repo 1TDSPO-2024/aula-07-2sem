5f6b02a0d47fc2162282421933f65391e0d07aae
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-28 23:18:13 -0300

    MALDIçÂO DO CP corrigida

--- aula07/src/routes/EditarProdutos/index.tsx
+++ aula07/src/routes/EditarProdutos/index.tsx
@@ -1,4 +1,15 @@
 import { useParams } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
+
+
+type Lista = {
+  id: number;
+  nome: string;
+  preco: number;
+  qtd: number;
+  descricao: string;
+  imagem: string;
+}
 
 export default function EditarProdutos() {
 
@@ -12,11 +23,30 @@ export default function EditarProdutos() {
   // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
   const { id } = useParams();
 
+  let nr: number = 0;
+
+  if (id != undefined) {
+    nr = parseInt(id);
+  } else {
+    nr = 0;
+  }
+
+  let produtoPesquisado;
+
+  produtoPesquisado = listaProdutos.find((produto) => {
+    if (produto.id == nr) {
+      return produto;
+    }
+  });
+
+
   return (
     <div>
       <h1>Olá, mundo sou o EditarProdutos!</h1>
       <div>
         <h2>ID1: {id}</h2>
+        <h2>{produtoPesquisado?.id}</h2>
+        <h2>{produtoPesquisado?.nome}</h2>
       </div>
     </div>
   );
--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,4 +1,5 @@
-
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
 
 export default function Produtos() {
 
@@ -8,6 +9,39 @@ export default function Produtos() {
   return (
     <div>
       <h1>Olá, mundo sou o Produtos!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Descrição</th>
+            <th>Imagem</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.descricao}</td>
+              <td>{produto.imagem}</td>
+              <td> <Link to={`/editar/produtos/${produto.id}`}>Editar</Link> </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={6}>
+              <span>Quantidade de produtos: {listaProdutos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   );
 }
